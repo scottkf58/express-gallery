669aff9732eeb06ffa9f1f8b76d336cbafdc630c
Scott Fujimoto <scottfujimoto@Scotts-MacBook-Pro.local>
2017-08-05 19:54:08 -1000

    working on handlebars

--- routes/gallery.js
+++ routes/gallery.js
@@ -1,12 +1,91 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../models');
+const Photo = db.Photo;
+
+router.route('/gallery/new')
+  .get( (req, res) => {
+    console.log('new');
+    res.render('/new');
+  });
 
 
+// Get/view gallery
+router.route('/')
+.get( (req, res) => {
+  Photo.findAll()
+    .then( (gallery) => {
+      console.log(gallery);
+      res.render('index', {gallery});
+    })
+    .catch( (err) => {
+      console.log(err);
+    });
+  });
 
 
+// POST to gallery
+router.route('/gallery')
+.post( (req, res) => {
+  Photo.create({
+    author: req.body.author,
+    link: req.body.link,
+    description: req.body.description
+  })
+    .then( (data) => {
+      console.log(data);
+      res.end();
+    })
+    .catch( (err) => {
+      console.log(err);
+    });
+  });
 
+// Get by id
+router.route('/gallery/:id')
+.get( (req, res) => {
+  Photo.findById(parseInt(req.params.id))
+    .then( (getPhoto) => {
+      res.render('getPhoto', {getPhoto});
+    })
+    .catch( (err) => {
+      console.log(err);
+    });
+  })
 
+// Edit id
+.put( (req, res) => {
+  Photo.update({
+    author: req.body.author,
+    link: req.body.link,
+    description: req.body.description
+  }, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then( (data) => {
+    res.end();
+  })
+  .catch( (err) => {
+    console.log(err);
+  });
+})
 
+// Delete by id
+.delete( (req, res) => {
+  Photo.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then( (data) => {
+    res.end();
+  })
+  .catch( (err) => {
+    console.log(err);
+  });
+});
 
 
 
--- server.js
+++ server.js
@@ -8,19 +8,20 @@ const galleryRouter = require('./routes/gallery.js');
 const app = express();
 app.use(bp.urlencoded());
 
+app.use('/', galleryRouter);
+
 const hbs = exphbs.create( {
   defaultLayout : 'main',
   extname : 'hbs'
 });
 
-app.engine( 'hbs', hbs.engine );
-app.set( 'view engine', 'hbs' );
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
 
 const PORT = process.envPORT || 3000;
 
 
 const server = app.listen(PORT, () => {
-  // Sync models folder
   db.sequelize.sync();
   console.log(`Server running on ${PORT}`);
 })
